2c1872dec94ae5f0cd97283bf4a2bede2b5d0115
Aleksandr Piletskii <108733729+vivlol@users.noreply.github.com>
2022-11-15 13:27:30 +0300

    Delete script.js

--- script.js
+++
@@ -1,2 +0,0 @@
-var numRand = Math.floor(Math.random()*361)
-$('style').append('@keyframes spinningH{ from{transform:rotateX(0deg) rotateY(0deg);}to{transform: rotateX('+numRand+'deg) rotateY('+numRand+'deg);}}');
